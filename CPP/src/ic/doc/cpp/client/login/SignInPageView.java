dd29cba0a7200caed6abae3de051f5a4d0c720f7
Yan <yan@wen.com>
2011-12-06 21:24:08 +0000

    SignIn imported

--- CPP/src/ic/doc/cpp/client/login/SignInPageView.java
+++ CPP/src/ic/doc/cpp/client/login/SignInPageView.java
@@ -9,133 +9,135 @@ import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.smartgwt.client.types.TitleOrientation;
+import com.smartgwt.client.widgets.HTMLPane;
+import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.Label;
+import com.smartgwt.client.widgets.form.DynamicForm;
+import com.smartgwt.client.widgets.form.fields.FormItem;
+import com.smartgwt.client.widgets.form.fields.PasswordItem;
+import com.smartgwt.client.widgets.form.fields.SelectItem;
+import com.smartgwt.client.widgets.form.fields.TextItem;
+import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.VLayout;
 
 public class SignInPageView extends ViewImpl implements
 		SignInPagePresenter.MyView {
 
-	 private static String html = "<div>\n"
-			    + "<table align=\"center\">\n"
-			    + "  <tr>\n" + "<td> </td>\n" + "<td> </td>\n" + "</tr>\n"
-			    + "  <tr>\n" + "<td> </td>\n" + "<td> </td>\n" + "</tr>\n"
-			    + "  <tr>\n" + "<td> </td>\n" + "<td> </td>\n" + "</tr>\n"
-			    + "  <tr>\n"    
-			    + "    <td colspan=\"2\" style=\"font-weight:bold;\">Student Sign In</td>\n"
-			    + "  </tr>\n"
-			    + "  <tr>\n"
-			    + "    <td>User name</td>\n"
-			    + "    <td id=\"userNameFieldContainer\"></td>\n"    
-			    + "  </tr>\n" 
-			    + "  <tr>\n"
-			    + "    <td>Password</td>\n"
-			    + "    <td id=\"passwordFieldContainer\"></td>\n"    
-			    + "  </tr>\n" 
-			    + "  <tr>\n"
-			    + "    <td></td>\n"
-			    + "    <td id=\"signInButtonContainer\"></td>\n"  
-			    + "  </tr>\n"     
-			    + "  <tr>\n" + "<td> </td>\n" + "<td> </td>\n" + "</tr>\n"
-			    + "  <tr>\n"
-			    + "    <td colspan=\"2\">Forget your password?</td>\n"
-			    + "  </tr>\n"
-			    + "  <tr>\n"
-			    + "    <td colspan=\"2\">Contact your CPP Administrator.</td>\n"
-			    + "  </tr>\n"    
-			    + "</table>\n"
-			    + "<table align=\"center\">\n"
-			    + "  <tr>\n" + "<td> </td>\n" + "<td> </td>\n" + "</tr>\n"
-			    + "  <tr>\n" + "<td> </td>\n" + "<td> </td>\n" + "</tr>\n"
-			    + "  <tr>\n" + "<td> </td>\n" + "<td> </td>\n" + "</tr>\n"
-			    + "  <tr>\n"    
-			    + "    <td colspan=\"2\" style=\"font-weight:bold;\">Company User Sign In</td>\n"
-			    + "  </tr>\n"
-			    + "  <tr>\n"
-			    + "    <td>User name</td>\n"
-			    + "    <td id=\"companyUserNameFieldContainer\"></td>\n"    
-			    + "  </tr>\n" 
-			    + "  <tr>\n"
-			    + "    <td>Password</td>\n"
-			    + "    <td id=\"companyUserPasswordFieldContainer\"></td>\n"    
-			    + "  </tr>\n" 
-			    + "  <tr>\n"
-			    + "    <td></td>\n"
-			    + "    <td id=\"companyUserSignInButtonContainer\"></td>\n"  
-			    + "  </tr>\n"     
-			    + "  <tr>\n" + "<td> </td>\n" + "<td> </td>\n" + "</tr>\n"
-			    + "  <tr>\n"
-			    + "    <td colspan=\"2\">Forget your password?</td>\n"
-			    + "  </tr>\n"
-			    + "  <tr>\n"
-			    + "    <td colspan=\"2\">Contact your CPP Administrator.</td>\n"
-			    + "  </tr>\n"    
-			    + "</table>\n"
-			    + "</div>\n";
-
-	 HTMLPanel panel = new HTMLPanel(html);
-
-	 private final TextBox userNameField, companyUserNameField;
-	 private final PasswordTextBox passwordField, companyUserPasswordField;
-	 private final Button signInButton, companyUserSignInButton;
+	VLayout mainLayout;
+	private TextItem userNameField;
+	private PasswordItem passwordField;
+	private SelectItem userType;
+	private IButton signInButton;
+	private TextItem userNameField2;
+	private PasswordItem passwordField2;
+	private SelectItem userType2;
+	private IButton regButton;
 
 	 @Inject
 	 public SignInPageView() {
-		 userNameField = new TextBox();
-		 passwordField = new PasswordTextBox();
-		 signInButton = new Button("Sign in");
+			mainLayout = new VLayout();
+			mainLayout.setHeight100();
+			mainLayout.setWidth100();
+			
+			HLayout topPanel = new HLayout();
+			topPanel.setBackgroundColor("#0B43C4");
+			topPanel.setHeight("10%");
+			
+			Label CppHeader = new Label("<font size='6' color='white'>Cooperative Partnership Programme</font>");
+			CppHeader.setWidth("150%");
 
-		 userNameField.setText("DuZhouzhou");
-		 passwordField.setText("N0More$ecret");
-		 
-		 companyUserNameField = new TextBox();
-		 companyUserPasswordField = new PasswordTextBox();
-		 companyUserSignInButton = new Button("Sign in");
-		 
-		 companyUserNameField.setText("TestCompany");
-		 companyUserPasswordField.setText("N0More$ecret");
-
-		 panel.add(userNameField, "userNameFieldContainer");
-		 panel.add(passwordField, "passwordFieldContainer");
-		 panel.add(signInButton, "signInButtonContainer");
-		 
-		 panel.add(companyUserNameField, "companyUserNameFieldContainer");
-		 panel.add(companyUserPasswordField, "companyUserPasswordFieldContainer");
-		 panel.add(companyUserSignInButton, "companyUserSignInButtonContainer");
+			DynamicForm loginForm = new DynamicForm();
+	        userNameField = new TextItem();  
+	        userNameField.setTitle("<font size='3' color='white'>Username</font>");  
+	        userNameField.setRequired(true);  
+	  
+	        passwordField = new PasswordItem();  
+	        passwordField.setTitle("<font size='3' color='white'>Password</font>");  
+	        passwordField.setRequired(true);  
+	        
+	        userType = new SelectItem();
+	        userType.setTitle("<font size='3' color='white'>User Type</font>");
+	        userType.setValueMap("Admin","Student","Company");
+	        
+	        signInButton = new IButton("Log In");
+	        
+			loginForm.setWidth100();
+	        loginForm.setFields(new FormItem[]{userNameField,passwordField,userType} );
+	        loginForm.setTitleOrientation(TitleOrientation.TOP);
+	        loginForm.setColWidths(new Object[]{"30%","30%","30%"});
+	        loginForm.setNumCols(3);
+	        
+			topPanel.addMember(CppHeader);
+	        topPanel.addMember(loginForm);
+	        topPanel.addMember(signInButton);
+			
+	        HLayout southPanel = new HLayout();
+	        HTMLPane CPPdescription = new HTMLPane();
+	        VLayout regPanel = new VLayout();
+	        
+			Label regWelcome = new Label("<font size='4' color='black'>Welcome to register</font>");
+			DynamicForm regForm = new DynamicForm();
+			
+	        userNameField2 = new TextItem();  
+	        userNameField2.setTitle("<font size='2' color='black'>Username</font>");  
+	        userNameField2.setRequired(true);  
+	  
+	        passwordField2 = new PasswordItem();  
+	        passwordField2.setTitle("<font size='2' color='black'>Password</font>");  
+	        passwordField2.setRequired(true);  
+	        
+	        userType2 = new SelectItem();
+	        userType2.setTitle("<font size='2' color='black'>UserType</font>");
+	        userType2.setValueMap("Admin","Student","Company");
+			
+	        regButton = new IButton("Registrate");
+	        regForm.setFields(new FormItem[]{userNameField2,passwordField2,userType2} );
+			
+	        regPanel.addMember(regWelcome);
+	        regPanel.addMember(regForm);
+	        regPanel.addMember(regButton);
+	        
+			southPanel.addMember(CPPdescription);
+			southPanel.addMember(regPanel);
+			
+			mainLayout.addMember(topPanel);
+			mainLayout.addMember(southPanel);
+			mainLayout.draw();
 	 }
 
 	 @Override
 	 public Widget asWidget() {
-		 return panel;
+		 return mainLayout;
 	 }
 
 	 @Override
 	 public String getUserName() {
-		 return userNameField.getText();
+		 return userNameField.getValueAsString();
 	 }
   
 	 @Override
 	 public String getPassword() {
-		 return passwordField.getText();
+		 return passwordField.getValueAsString();
 	 }  
 
 	 
 	 @Override
 	 public String getCompanyUserName() {
-		 return companyUserNameField.getText();
+		 return getUserName();
 	 }
 	 @Override
 	 public String getCompanyUserPassword() {
-		 return companyUserPasswordField.getText();
+		 return getPassword();
 	 }
 
 	@Override
 	 public void resetAndFocus() {
-		 userNameField.setFocus(true);
-		 userNameField.selectAll();
 	 }
 	
 	@Override
 	 public void resetAndFocusCompanyUser() {
-		 companyUserNameField.setFocus(true);
-		 companyUserNameField.selectAll();
+		
 	 }
 
 
--- CPP/src/ic/doc/cpp/server/domain/EventCategory_.java
+++ CPP/src/ic/doc/cpp/server/domain/EventCategory_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.1.v20111018-r10243", date="2011-12-05T16:44:03")
+@Generated(value="EclipseLink-2.3.1.v20111018-r10243", date="2011-12-06T20:59:45")
 @StaticMetamodel(EventCategory.class)
 public class EventCategory_ { 
 
